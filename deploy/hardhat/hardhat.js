109315dcbcaeae1afeb2990af4bde36effae03cd
Siew Chong Che <siewchongche@gmail.com>
2021-03-13 14:23:47 +0800

    Reset mainnet fork before each deploy.

--- deploy/hardhat/hardhat.js
+++ deploy/hardhat/hardhat.js
@@ -1,17 +1,9 @@
-const { network } = require("hardhat");
-
-module.exports = async () => {
-  await network.provider.request({
-    method: "hardhat_reset",
-    params: [
-      {
-        forking: {
-          jsonRpcUrl: process.env.ALCHEMY_URL_MAINNET,
-          blockNumber: 11960000,
-        },
-      },
-    ],
-  });
-};
-module.exports.tags = ["hardhat"]
-module.exports.dependencies = ["hardhat_USDT", "hardhat_USDC", "hardhat_DAI", "hardhat_TUSD"]
+module.exports = async () => {};
+module.exports.tags = ["hardhat"];
+module.exports.dependencies = [
+  "hardhat_reset",
+  "hardhat_USDT",
+  "hardhat_USDC",
+  "hardhat_DAI",
+  "hardhat_TUSD",
+];
