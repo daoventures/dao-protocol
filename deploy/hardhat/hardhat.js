45c284d2fa33e5e6d4fe00ee18ff1cd110d38289
Siew Chong Che <siewchongche@gmail.com>
2021-03-13 12:54:35 +0800

    File re-allocate adjustment.

---
+++ deploy/hardhat/hardhat.js
@@ -0,0 +1,6 @@
+const { network } = require("hardhat");
+
+module.exports = async () => {};
+module.exports.tags = ["hardhat"];
+// module.exports.dependencies = ["hardhat_USDT", "hardhat_USDC", "hardhat_DAI", "hardhat_TUSD"]
+module.exports.dependencies = ["hardhat_reset", "hardhat_USDT"];
---
+++ deploy/hardhat/hardhat_deploy.js
@@ -0,0 +1,47 @@
+const { ethers, network } = require("hardhat");
+const { mainnet: network_ } = require("../../addresses");
+
+const tokenAddress = network_.USDT.tokenAddress;
+const unlockedAddress = "0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8";
+
+module.exports = async ({ deployments }) => {
+  const { deploy } = deployments;
+  const [deployer] = await ethers.getSigners();
+
+  const cfUSDT = await deploy("CompoundFarmerUSDT", {
+    from: deployer.address,
+    args: [
+      network_.USDT.tokenAddress,
+      network_.USDT.cTokenAddress,
+      network_.USDT.compTokenAddress,
+      network_.USDT.comptrollerAddress,
+      network_.USDT.uniswapRouterAddress,
+      network_.USDT.WETHAddress,
+    ],
+  });
+
+  const dvlUSDT = await deploy("DAOVaultLowUSDT", {
+    from: deployer.address,
+    args: [network_.USDT.tokenAddress, cfUSDT.address],
+  });
+
+  const cfUSDTContract = await ethers.getContract("CompoundFarmerUSDT");
+  await cfUSDTContract.setVault(dvlUSDT.address);
+
+  // Transfer token from unlocked account to deployer
+  await network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [unlockedAddress],
+  });
+  const [senderSigner, _] = await ethers.getSigners();
+  const unlockedSigner = await ethers.getSigner(unlockedAddress);
+  ABI = [
+    "function transfer(address, uint) external",
+    "function balanceOf(address) external view returns (uint)",
+  ];
+  const tokenContract = new ethers.Contract(tokenAddress, ABI, senderSigner);
+  await tokenContract
+    .connect(unlockedSigner)
+    .transfer(senderSigner.address, tokenContract.balanceOf(unlockedAddress));
+};
+module.exports.tags = ["hardhat_USDT"]
