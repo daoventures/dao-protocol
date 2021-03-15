6e53da31664b038f7e2d2f19deca83849f9c2547
Siew Chong Che <siewchongche@gmail.com>
2021-03-15 22:30:31 +0800

    Move show addresses task to script.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,29 +4,6 @@ require("hardhat-deploy");
 require("hardhat-deploy-ethers");
 require("dotenv").config();
 
-task("check", "Check addresses for all deployed contracts", async () => {
-  const yfUSDTContract = await deployments.get("YearnFarmerUSDTv2");
-  const dvmUSDTContract = await deployments.get("DAOVaultMediumUSDT");
-  const yfUSDCContract = await deployments.get("YearnFarmerUSDCv2");
-  const dvmUSDCContract = await deployments.get("DAOVaultMediumUSDC");
-  const yfDAIContract = await deployments.get("YearnFarmerDAIv2");
-  const dvmDAIContract = await deployments.get("DAOVaultMediumDAI");
-  const yfTUSDContract = await deployments.get("YearnFarmerTUSDv2");
-  const dvmTUSDContract = await deployments.get("DAOVaultMediumTUSD");
-
-  console.log("Yearn-Farmer USDT v2 address: ", yfUSDTContract.address);
-  console.log("DAO Vault Medium USDT address: ", dvmUSDTContract.address);
-  console.log("");
-  console.log("Yearn-Farmer USDC v2 address: ", yfUSDCContract.address);
-  console.log("DAO Vault Medium USDC address: ", dvmUSDCContract.address);
-  console.log("");
-  console.log("Yearn-Farmer DAI v2 address: ", yfDAIContract.address);
-  console.log("DAO Vault Medium DAI address: ", dvmDAIContract.address);
-  console.log("");
-  console.log("Yearn-Farmer TUSD v2 address: ", yfTUSDContract.address);
-  console.log("DAO Vault Medium TUSD address: ", dvmTUSDContract.address);
-})
-
 module.exports = {
   networks: {
     hardhat: {
@@ -39,14 +16,14 @@ module.exports = {
       url: process.env.ALCHEMY_URL_MAINNET,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
-    // rinkeby: {
-    //   url: process.env.ALCHEMY_URL_RINKEBY,
-    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
-    // },
+    rinkeby: {
+      url: process.env.ALCHEMY_URL_RINKEBY,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
   },
-  // etherscan: {
-  //   apiKey: process.env.ETHERSCAN_API_KEY,
-  // },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
   solidity: {
     compilers: [
       {
