8771cd67743e2958f8d8bde8f672cddf5b119b38
Siew Chong Che <siewchongche@gmail.com>
2021-03-16 00:25:43 +0800

    Comment out reset function for mainnet fork deployment testing. Console out text for deploy successful on mainnet fork.

--- deploy/hardhat/hardhat.js
+++ deploy/hardhat/hardhat.js
@@ -1,7 +1,9 @@
-module.exports = async () => {};
+module.exports = async () => {
+  console.log("All contracts had deployed successfully on Mainnet fork.")
+};
 module.exports.tags = ["hardhat"];
 module.exports.dependencies = [
-  "hardhat_reset",
+  // "hardhat_reset",
   "hardhat_USDT",
   "hardhat_USDC",
   "hardhat_DAI",
