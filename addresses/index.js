bfc4f87b6a414a54bf4fc998b6ea0c50cd3d72d3
Siew Chong Che <siewchongche@gmail.com>
2021-03-18 01:11:47 +0800

    Update addresses.

--- addresses/index.js
+++ addresses/index.js
@@ -1,12 +1,26 @@
 module.exports = {
   mainnet: {
-    USDT: {
-      tokenAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-      cTokenAddress: "0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9",
+    GLOBAL: {
       compTokenAddress: "0xc00e94Cb662C3520282E6f5717214004A7f26888",
       comptrollerAddress: "0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B",
       uniswapRouterAddress: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
       WETHAddress: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    },
+    USDT: {
+      tokenAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+      cTokenAddress: "0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9",
+    },
+    USDC: {
+      tokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      cTokenAddress: "0x39AA39c021dfbaE8faC545936693aC917d5E7563",
+    },
+    // ETH: {
+    //   tokenAddress: "",
+    //   cTokenAddress: "0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5",
+    // },
+    DAI: {
+      tokenAddress: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      cTokenAddress: "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643",
     },
   },
   kovan: {
