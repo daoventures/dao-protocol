6d52c38b12596d99e08fa3c441b9fb52d4c06516
Siew Chong Che <siewchongche@gmail.com>
2021-03-19 00:39:50 +0800

    Miscellaneous.

--- deploy/hardhat/hardhat.js
+++ deploy/hardhat/hardhat.js
@@ -1,5 +1,8 @@
-const { network } = require("hardhat");
-
 module.exports = async () => {};
 module.exports.tags = ["hardhat"];
-module.exports.dependencies = ["hardhat_reset", "hardhat_deploy_USDT", "hardhat_deploy_USDC", "hardhat_deploy_DAI"];
+module.exports.dependencies = [
+  "hardhat_reset",
+  "hardhat_deploy_USDT",
+  "hardhat_deploy_USDC",
+  "hardhat_deploy_DAI",
+];
