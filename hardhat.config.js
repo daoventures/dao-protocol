db9c9c738e26d627b82daf97cf2fbc0177c0dec2
seshanthS <seshanth.s@hotmail.com>
2021-07-02 10:48:12 +0530

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,42 @@
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("@nomiclabs/hardhat-solhint");
+require("dotenv").config();
+
+module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_URL_MAINNET,
+        // blockNumber: 11960000,
+      },
+    },
+    // mainnet: {
+    //   url: process.env.ALCHEMY_URL_MAINNET,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // },
+    // kovan: {
+    //   url: process.env.ALCHEMY_URL_KOVAN,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // },
+    // rinkeby: {
+    //   url: process.env.ALCHEMY_URL_RINKEBY,
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
+    // },
+  },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
